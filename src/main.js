67d1ec3c7f61c763c4aab21688bf9c66aa5b754d
Alex Kotliarskyi <alex.frantic@gmail.com>
2015-08-04 22:07:44 -0700

    Editor / preview setup

---
+++ src/main.js
@@ -0,0 +1,28 @@
+function run(code) {
+  code += 'React.render(<Example />, display);'
+  try {
+    code = JSXTransformer.transform(code).code;
+    eval(code);
+    display.style.opacity = 1;
+    error.innerHTML = '';
+  } catch(e) {
+    display.style.opacity = 0.2;
+    console.error(code);
+    console.error(e.message);
+    error.innerHTML = e.message;
+  }
+}
+
+window.addEventListener('load', function() {
+  var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    mode: 'javascript',
+    theme: 'neo',
+    tabSize: 2,
+    viewportMargin: Infinity
+  });
+  editor.focus();
+  run(editor.getValue());
+  editor.on('change', function() {
+    run(editor.getValue());
+  });
+});
